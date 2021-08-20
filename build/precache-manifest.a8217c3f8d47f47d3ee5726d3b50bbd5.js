edbab2373245ab1f307812354ffcf937ec7d2eba
bpinazmul18 <bpinazmul18@gmail.com>
2021-08-20 16:07:34 +0600

    for production

---
+++ build/precache-manifest.a8217c3f8d47f47d3ee5726d3b50bbd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6067c979a0959c4e4ea328409b8a302a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f23bd65104284867c66",
+    "url": "/static/css/main.ff45c5cb.chunk.css"
+  },
+  {
+    "revision": "bbb8de9a106f740fd614",
+    "url": "/static/js/2.492d9404.chunk.js"
+  },
+  {
+    "revision": "1e018907b7aa2bab8a5437faf893dc77",
+    "url": "/static/js/2.492d9404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f23bd65104284867c66",
+    "url": "/static/js/main.66d470d3.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/static/media/login-img.20a90984.svg"
+  },
+  {
+    "revision": "c114fda3847c2b52236772df2f17cd01",
+    "url": "/static/media/preloader.c114fda3.gif"
+  }
+]);
