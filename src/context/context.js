1140c8b4a20e70509f22d44db206aef8a9f27d4e
bpinazmul18 <bpinazmul18@gmail.com>
2021-08-29 14:59:18 +0600

    bug fix

--- src/context/context.js
+++ src/context/context.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
-// import mockUser from "./mockData.js/mockUser";
 import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
 import axios from "axios";
+import mockUser from "./mockData.js/mockUser";
 
 const rootUrl = "https://api.github.com";
 
 const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
-  const [githubUser, setGithubUser, ] = useState('bpinazmul18');
+  const [githubUser, setGithubUser, ] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
